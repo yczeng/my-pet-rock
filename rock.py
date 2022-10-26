0b9597ed3e5b7baa7a143fcec94107dcf2e3e7a0
Zachary Pitcher <zackpi@mit.edu>
2022-10-26 19:17:38 -0400

    ascii ⇝ ᴜ ɴ ɪ ᴄ ᴏ ᴅ ᴇ

--- rock.py
+++ rock.py
@@ -2,23 +2,22 @@
 
 import random
 
+
 def display_joke(jokes):
-	a = random.choice(jokes)
-	print (
-'  _'+ ('_' * (len(a) - 1))+ '\n' + 
-'< ' + a +' >\n' + 
-'  -'+ ('-' * (len(a) - 1))+ '\n' + 
-'      \\\n' +
-'       \\\n' + 
-'    ___________________\n'
-'   /                   \\\n'
-'  (       o     o       )\n'
-' (           u           )\n'
-'  (_____________________)\n')
+    a = random.choice(jokes)
+    print(
+        '╭─' + ('─' * len(a)) + '─╮\n' +
+        '│ ' + a + ' │\n' +
+        '╰◹' + ('─' * len(a)) + '─╯\n' +
+        '     ____________________\n'
+        '    ⎛⎛     ◜       ◝     ⎞\n'
+        '   ╱░│      ◔     ◔      │\n'
+        '  ⎛░░⎛         ◡          ╲\n'
+        '  ╲░░╲▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁⎠\n')
 
 
 with open('jokes.txt') as f:
-	content = f.readlines()
+    content = f.readlines()
 jokes = [x.strip() for x in content]
 
 display_joke(jokes)
